0ccc28cd59695d287dac2e8ed890afc327b4d052
akram <168238434+akramelassri@users.noreply.github.com>
2025-11-21 15:19:32 +0100

    Finish mvc v1
    Separated the database acess into a modele.php file now
    it's a simple mvc architecture

---
+++ Modele.php
@@ -0,0 +1,11 @@
+<?php
+
+$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8','bloguser','bloguserpw');
+
+function getBillets($bdd) {
+	$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
+	. ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+	. ' order by BIL_ID desc');
+
+	return $billets;
+}
