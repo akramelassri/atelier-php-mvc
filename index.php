cd5b7f31c9c43bf5347c0ad701a10637024e5591
akram <168238434+akramelassri@users.noreply.github.com>
2025-11-21 15:04:04 +0100

    Refactor to mvc v1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// Accès aux données
+$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'bloguser', 'bloguserpw');
+$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
+. ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+. ' order by BIL_ID desc');
+// Affichage
+require 'vueAccueil.php';
